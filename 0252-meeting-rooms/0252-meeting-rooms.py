e27e5840ec7965508ced87a3a8e61f89ad040a26
Sunil Choudhury <linuschoudhury@gmail.com>
2024-11-09 01:49:31 +0530

    Time: 3 ms (84.83%), Space: 13.4 MB (92.54%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        intervals.sort()
+        for i in range(len(intervals)-1):
+            if intervals[i][1]>intervals[i+1][0]:
+                return False
+        return True
+        
+        
